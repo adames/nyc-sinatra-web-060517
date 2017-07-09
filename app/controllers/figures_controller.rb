3790e370413cf6b724d64984d12f7210ad146071
adames <adameshh@gmail.com>
2017-07-08 23:33:52 -0400

    too late to leave accurate message

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,20 +1,38 @@
 class FiguresController < ApplicationController
 
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
-    erb :'/landmarks/new'
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
   end
 
   post '/figures' do
-    binding.pry
-    # {"figure"=>{"name"=>"Alex"}, "title"=>{"name"=>"The great"}, "landmark"=>{"name"=>""}, "submit"=>"Create New Figure"}
-
-    @title = Title.find_by_id(params[:titles])
-
-
-
-
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    elsif !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    redirect "/figures/#{@figure.id}"
   end
+
 
 end
--- app/models/figure.rb
+++ app/models/figure.rb
@@ -3,4 +3,15 @@ class Figure <ActiveRecord::Base
   has_many :figure_titles
   has_many :titles, through: :figure_titles
 
+  def title_ids=(params)
+    params.each do |title|
+      self.titles << Title.find(title.keys.first.to_i)
+    end
+  end
+
+  def landmark_ids=(params)
+    params.each do |landmark|
+      self.landmarks << Landmark.find(landmark.keys.first.to_i)
+    end
+  end
 end
